b0b9cb4326b01ee3b15e595f3c7eeea61f2d6a70
石述忠 <stone_me_06@163.com>
2018-05-20 11:26:06 +0800

    设备配置页面框架搭建：初步完成

--- src/components/DeviceCfg.vue
+++ src/components/DeviceCfg.vue
@@ -2,25 +2,21 @@
 <div :class="$style.module">
   <div :class="$style.title">创建或修改设备</div>
   <div :class="$style.content">
-    <div :class="$style.baseMsg">
-      <div :class="[$style.head, this.collapsed ? $style.collapsed : null]">
-        <div>基本信息</div>
-      </div>
+    <div :class="[$style.block, collapsedBasicMsg ? $style.collapsed : null]">
+      <div :class="$style.head" @click="collapsedBasicMsg = !collapsedBasicMsg"><div>基本信息</div></div>
+      <div :class="$style.body">内容</div>
     </div>
-    <div :class="$style.connectWay">
-      <div :class="[$style.head, this.collapsed ? $style.collapsed : null]">
-        <div :class="$style.content">接入方式</div>
-      </div>
+    <div :class="[$style.block, collapsedConnectWay ? $style.collapsed : null]">
+      <div :class="$style.head" @click="collapsedConnectWay = !collapsedConnectWay"><div>接入方式</div></div>
+      <div :class="$style.body">内容</div>
     </div>
-    <div :class="$style.deviceType">
-      <div :class="[$style.head, this.collapsed ? $style.collapsed : null]">
-        <div :class="$style.content">设备类型</div>
-      </div>
+    <div :class="[$style.block, collapsedDeviceType ? $style.collapsed : null]">
+      <div :class="$style.head" @click="collapsedDeviceType = !collapsedDeviceType"><div>设备类型</div></div>
+      <div :class="$style.body">内容</div>
     </div>
-    <div :class="$style.dataCfg">
-      <div :class="[$style.head, this.collapsed ? $style.collapsed : null]">
-        <div :class="$style.content">数据配置</div>
-      </div>
+    <div :class="[$style.block, collapsedDataCfg ? $style.collapsed : null]">
+      <div :class="$style.head" @click="collapsedDataCfg = !collapsedDataCfg"><div>数据配置</div></div>
+      <div :class="$style.body">内容</div>
     </div>
   </div>
 </div>
@@ -31,7 +27,10 @@ export default {
   name: 'device-cfg',
   data () {
     return {
-      collapsed: false
+      collapsedBasicMsg: false,
+      collapsedConnectWay: false,
+      collapsedDeviceType: false,
+      collapsedDataCfg: false
     }
   }
 }
@@ -49,7 +48,7 @@ export default {
     padding: 0.5rem;
   }
   & > .content {
-    & > * {
+    & > .block {
       position: relative;
       padding: 0.5rem;
       $border-color: rgba(197, 197, 197, 0.44);
@@ -74,13 +73,14 @@ export default {
       &:last-child::after {
         width: 100%;
       }
+      $ec-btn-size: 0.5rem;
       & > .head {
         display: flex;
         align-items: center;
-        &:first-child {
+        cursor: pointer;
+        & > :first-child {
           flex: auto;
         }
-        $ec-btn-size: 0.5rem;
         &::after {
           flex: none;
           content: '';
@@ -89,20 +89,19 @@ export default {
           $border-color: gray;
           border-top: 1px solid $border-color;
           border-right: 1px solid $border-color;
-          transform: translateX(-$ec-btn-size / 2) rotate(-45deg);
-        }
-        &.collapsed::after {
-          transform: translateX(-$ec-btn-size / 2) rotate(135deg);
+          transform: translate3d(-$ec-btn-size / 2, $ec-btn-size / 4, 0) rotate(-45deg);
         }
       }
-    }
-    & > .base-msg {
-    }
-    & > .connect-way {
-    }
-    & > .device-type {
-    }
-    & > .data-cfg {
+      & > .body {
+      }
+      &.collapsed {
+        & > .head::after {
+          transform: translate3d(-$ec-btn-size / 2, -$ec-btn-size / 4, 0) rotate(135deg);
+        }
+        & > .body {
+          display: none;
+        }
+      }
     }
   }
 }
